94dbb8598ffd2eb2cdb3021dc660c0cc4f82fb6b
Giuseppe Caruso <giuseppe.caruso@smartly.io>
2022-03-10 17:03:46 +0100

    add nesting routes

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
 
-export default class BandsBandRoute extends Route {}
+export default class BandsBandRoute extends Route {
+  model(params) {
+    // modelFor is a truly great method, which fetches the model
+    // of a parent route that had already been activated
+    let bands = this.modelFor('bands');
+    return bands.find((band) => band.id === params.id);
+  }
+}
